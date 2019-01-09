9144315b8f988225ac3094535aad3504b2157615
Tom Bell <tomb@tomb.io>
2019-01-09 18:45:17 +0000

    Add comments to driver functions

--- driver.go
+++ driver.go
@@ -10,16 +10,17 @@ type Driver interface {
 	UnmarkVersionAsExecuted(tx *sql.Tx, version string) error
 }
 
-// PostgresDriver ...
+// PostgresDriver is a PostgreSQL implementation of the Driver interface.
 type PostgresDriver struct{}
 
-// CreateVersionsTable ...
+// CreateVersionsTable creates the table for storing migrated versions if it
+// doesn't exist.
 func (d *PostgresDriver) CreateVersionsTable(db *sql.DB) error {
 	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS database_versions(version TEXT);`)
 	return err
 }
 
-// HasVersionBeenExecuted ...
+// HasVersionBeenExecuted checks if the given version has been executed.
 func (d *PostgresDriver) HasVersionBeenExecuted(db *sql.DB, version string) (bool, error) {
 	var count int
 
@@ -30,13 +31,13 @@ func (d *PostgresDriver) HasVersionBeenExecuted(db *sql.DB, version string) (boo
 	return count > 0, nil
 }
 
-// MarkVersionAsExecuted ...
+// MarkVersionAsExecuted marks a given version as been executed.
 func (d *PostgresDriver) MarkVersionAsExecuted(tx *sql.Tx, version string) error {
 	_, err := tx.Exec("INSERT INTO database_versions (version) VALUES ($1)", version)
 	return err
 }
 
-// UnmarkVersionAsExecuted ...
+// UnmarkVersionAsExecuted unmarks a given version as been executed.
 func (d *PostgresDriver) UnmarkVersionAsExecuted(tx *sql.Tx, version string) error {
 	_, err := transaction.Exec("DELETE FROM database_versions WHERE version=$1", version)
 	return err
